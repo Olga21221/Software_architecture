3f30f24c2262225fc24d84fd1ebe328268cc6be1
Olga <olgaolga21221@mail.ru>
2023-09-30 19:45:16 +0300

    homework1

---
+++ Homework_1/PoligonalModel.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoligonalModel {
+    public List<Poligon> Poligons;
+    public List<Texture> Textures;
+
+    public PoligonalModel(List<Texture> textures) {
+        Textures = textures;
+        Poligons = new ArrayList<>();
+    }
+}
